f6924da63682bd6b906383c6f302bbbf130d28fc
Rhett Newton <rhett.newton@gmail.com>
2021-10-11 22:57:22 -0500

    Add method to create board data

--- scenes/play_field.gd
+++ scenes/play_field.gd
@@ -4,8 +4,21 @@ export var block_size := 8
 export var width := 10
 export var height := 20
 
+onready var _board_state := []
+
 func _ready() -> void:
-	var tetromino_factory = TetrominoFactory.new()
-	var rand_tetromino = tetromino_factory.get_random()
-	add_child(rand_tetromino)
+	randomize()
+	reset_board_state()
+	add_child(_get_random_tetromino())
 	pass
+
+func reset_board_state():
+	_board_state = []
+	for i in range(height):
+		var row := []
+		_board_state.append(row)
+		for j in range(width):
+			row.append(false)
+
+func _get_random_tetromino():
+	return TetrominoFactory.get_random()
