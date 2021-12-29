8823a90ce7708812e0e754fad7dc255bb7d9ddb8
Rhett Newton <rhett.newton@gmail.com>
2021-12-28 22:01:41 -0600

    Add methods to get each tetromino

--- scenes/tetromino_factory.gd
+++ scenes/tetromino_factory.gd
@@ -15,3 +15,31 @@ static func get_random() -> Tetromino:
 	var index = rand_range(0, _packed_scenes.size())
 	var scene: PackedScene = _packed_scenes[index]
 	return scene.instance() as Tetromino
+
+
+static func get_i() -> Tetromino:
+	return _packed_scenes[0].instance() as Tetromino
+
+
+static func get_j() -> Tetromino:
+	return _packed_scenes[1].instance() as Tetromino
+
+
+static func get_l() -> Tetromino:
+	return _packed_scenes[2].instance() as Tetromino
+
+
+static func get_o() -> Tetromino:
+	return _packed_scenes[3].instance() as Tetromino
+
+
+static func get_s() -> Tetromino:
+	return _packed_scenes[4].instance() as Tetromino
+
+
+static func get_t() -> Tetromino:
+	return _packed_scenes[5].instance() as Tetromino
+
+
+static func get_z() -> Tetromino:
+	return _packed_scenes[6].instance() as Tetromino
