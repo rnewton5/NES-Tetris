f2fd24af9d68eb67197e0c04414e462a4a8d0cfa
Rhett Newton <rhett.newton@gmail.com>
2021-12-31 21:48:28 -0600

    Fix block positions

--- scenes/tetrominos/tetromino_l.gd
+++ scenes/tetrominos/tetromino_l.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 		[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(0, 1)],
 		[Vector2(1, -1), Vector2(1, 0), Vector2(1, 1), Vector2(0, -1)],
 		[Vector2(2, 0), Vector2(1, 0), Vector2(0, 0), Vector2(2, -1)],
-		[Vector2(1, 1), Vector2(1, 0), Vector2(1, -1), Vector2(2, 11)],
+		[Vector2(1, 1), Vector2(1, 0), Vector2(1, -1), Vector2(2, 1)],
 	]
 	update_blocks()
